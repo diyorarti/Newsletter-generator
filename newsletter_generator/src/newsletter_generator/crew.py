06795277cb73547b1dddad4fbb06084adfd2c783
diyorarti <diyorarti@example.com>
2024-09-10 08:12:39 +0500

    Newsletter Generator updated

--- newsletter_generator/src/newsletter_generator/crew.py
+++ newsletter_generator/src/newsletter_generator/crew.py
@@ -1,6 +1,10 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
-
+from newsletter_generator.tools.search import SearchAndContents, FindSimilar, GetContents
+from datetime import datetime
+from langchain_anthropic import ChatAnthropic
+from langchain_groq import ChatGroq
+from datetime import datetime
 # Uncomment the following line to use an example of a custom tool
 # from newsletter_generator.tools.custom_tool import MyCustomTool
 
@@ -8,46 +12,77 @@ from crewai.project import CrewBase, agent, crew, task
 # from crewai_tools import SerperDevTool
 
 @CrewBase
-class NewsletterGeneratorCrew():
-	"""NewsletterGenerator crew"""
-	agents_config = 'config/agents.yaml'
-	tasks_config = 'config/tasks.yaml'
+class NewsletterGeneratorCrew:
+    """NewsletterGenerator crew"""
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
 
-	@agent
-	def researcher(self) -> Agent:
-		return Agent(
-			config=self.agents_config['researcher'],
-			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
-			verbose=True
-		)
+	def llm(self):
+        llm = ChatAnthropic(model_name="claude-3-sonnet-20240229", max_tokens=4096)
+        # llm = ChatGroq(model="llama3-70b-8192")
+        # llm = ChatGroq(model="mixtral-8x7b-32768")
+        # llm = ChatGoogleGenerativeAI(google_api_key=os.getenv("GOOGLE_API_KEY"))
 
-	@agent
-	def reporting_analyst(self) -> Agent:
-		return Agent(
-			config=self.agents_config['reporting_analyst'],
-			verbose=True
-		)
+        return llm
 
-	@task
-	def research_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['research_task'],
-		)
+    @agent
+    def researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config["researcher"],
+            tools=[SearchAndContents(), FindSimilar(), GetContents()],
+            verbose=True,
+			llm=self.llm()
+        )
 
-	@task
-	def reporting_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
-		)
+    @agent
+    def editor(self) -> Agent:
+        return Agent(
+            config=self.agents_config["editor"],
+            verbose=True,
+            tools=[SearchAndContents(), FindSimilar(), GetContents()],
+			llm=self.llm()
+        )
 
-	@crew
-	def crew(self) -> Crew:
-		"""Creates the NewsletterGenerator crew"""
-		return Crew(
-			agents=self.agents, # Automatically created by the @agent decorator
-			tasks=self.tasks, # Automatically created by the @task decorator
-			process=Process.sequential,
-			verbose=True,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
-		)
+    @agent
+    def designer(self) -> Agent:
+        return Agent(
+            config=self.agents_config["designer"],
+            verbose=True,
+            allow_delegation=False,
+			llm=self.llm()
+        )
+
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["research_task"],
+            agent=self.researcher(),
+            output_file=f"logs/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_research_task.md",
+        )
+
+    @task
+    def edit_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["edit_task"],
+            agent=self.editor(),
+            output_file=f"logs/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_edit_task.md",
+        )
+
+    @task
+    def newsletter_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["newsletter_task"],
+            agent=self.designer(),
+            output_file=f"logs/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_newsletter_task.html",
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the NewsletterGenerator crew"""
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+            # process=Process.hierarchical, # In case you want to use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
--- newsletter_generator/src/newsletter_generator/main.py
+++ newsletter_generator/src/newsletter_generator/main.py
@@ -1,54 +1,18 @@
 #!/usr/bin/env python
-import sys
 from newsletter_generator.crew import NewsletterGeneratorCrew
 
-# This main file is intended to be a way for your to run your
-# crew locally, so refrain from adding necessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
+def load_html_template(): 
+    with open('src/newsletter_generator/config/newsletter_template.html', 'r') as file:
+        html_template = file.read()
+        
+    return html_template
+
 
 def run():
-    """
-    Run the crew.
-    """
+    # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
-        'topic': 'AI LLMs'
+        'topic': input('Enter the topic for yout newsletter: '),
+        'personal_message': input('Enter a personal message for your newsletter: '),
+        'html_template': load_html_template()
     }
     NewsletterGeneratorCrew().crew().kickoff(inputs=inputs)
-
-
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs"
-    }
-    try:
-        NewsletterGeneratorCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        NewsletterGeneratorCrew().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs"
-    }
-    try:
-        NewsletterGeneratorCrew().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
